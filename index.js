7300a8c5dc711145e195c22569a05bca97868db4
hdekjsne <hdekjsne@gmail.com>
2023-05-30 04:57:53 +0300

    second step

--- index.js
+++ index.js
@@ -22,6 +22,26 @@ export default function solution(content){
     openingVariation: 16,
   }
 
+  const { 
+    id,
+    ifRated,
+    turns,
+    status,
+    winner,
+    time,
+    whiteId,
+    whiteRating,
+    blackId,
+    blackRating,
+    moves,
+    openingCode,
+    openingMoves,
+    openingFullname,
+    openingShortname,
+    openingResponse,
+    openingVariation
+  } = keys;
+
   const processed = content
     .split('\r\n')
     .slice(1)
@@ -30,14 +50,20 @@ export default function solution(content){
   // first step
   const totalGamesCount = processed.length;
   // second step
+  const ratedTrue = processed
+    .filter((game) => game[ifRated] === 'TRUE')
+    .map((game) => game[ifRated]);
+
+  const truesPercentage = (ratedTrue.length / processed.length).toFixed(2) * 100;
+  const falsesPercentage = 100 - truesPercentage;
   // third step
   // fourth step
   // fifth step
 
-  const finalString = `Total games count: ${totalGamesCount}`;
+  const finalString = `Total games count: ${totalGamesCount}
+  The approximate amount of: rated games - ${truesPercentage}%, not rated games- ${falsesPercentage}%`;
 
   console.log(finalString);
-  console.log(processed);
   // node bin/app.js __fixtures__/chess_games.csv
   // END
 }
